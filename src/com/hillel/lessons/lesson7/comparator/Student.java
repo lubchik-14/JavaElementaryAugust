89f96e9a0f30feb5d4ed2e029e127348bd38877c
Luba Kruglaya <lubchik.14@gmail.com>
2019-10-03 15:56:05 +0300

    Implemented Comparator<Student> with Lambda Expression (lesson 7, point 1)

--- src/com/hillel/lessons/lesson7/comparator/Student.java
+++ src/com/hillel/lessons/lesson7/comparator/Student.java
@@ -12,33 +12,20 @@ public class Student {
     }
 
     public static Comparator<Student> compareByAge() {
-        return new Comparator<Student>() {
-            @Override
-            public int compare(Student o1, Student o2) {
-                return o1.age - o2.age;
-            }
-        };
+        return ((o1, o2) -> o1.age - o2.age);
     }
 
     public static Comparator<Student> compareByName(){
-        return new Comparator<Student>() {
-            @Override
-            public int compare(Student o1, Student o2) {
-                return o1.name.compareTo(o2.name);
-            }
-        };
+        return (o1, o2) -> o1.name.compareTo(o2.name);
     }
 
     public static Comparator<Student> compareByNameByAge(){
-        return new Comparator<Student>() {
-            @Override
-            public int compare(Student o1, Student o2) {
-                int result = compareByName().compare(o1, o2);
-                if (result == 0) {
-                    return compareByAge().compare(o1, o2);
-                }
-                return result;
+        return (o1, o2) -> {
+            int result = compareByName().compare(o1, o2);
+            if (result == 0) {
+                return compareByAge().compare(o1, o2);
             }
+            return result;
         };
     }
 
